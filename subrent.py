5464cfca207df74107c187489c67b2f0c469bda7
Deviur <deviur@yandex.ru>
2022-12-27 22:07:47 +0300

    Add args for subrent.py

--- subrent.py
+++ subrent.py
@@ -1,9 +1,48 @@
 #!/usr/bin/env python
+import argparse
+import logging
 
-def main(args):
-    print(args)
 
+def _parse(argv):
+    parser = argparse.ArgumentParser(description='Get an income report for a day')
+    parser.add_argument('-D', '--debug', action='store_true', default=False, help="set debug logging level")
+    subparsers = parser.add_subparsers(dest='command', required=True)
+    reset_parser = subparsers.add_parser('reset', help="Reset Subrent's database")
+    import_parser = subparsers.add_parser('import', help="Import ZenMoney's json file to Subrent's database")
+    import_parser.add_argument('json_filename', action='store', help="ZenMoney's json file")
+    report_parser = subparsers.add_parser('report', help="Get a report")
+    report_parser.add_argument('date', action='store')
+    return parser.parse_args(argv[1:])
+
+
+def _reset(options):
+    logging.info('Starting a reset...')
+
+
+def _import_json_file(options):
+    logging.info('Starting an import...')
+    logging.debug(f'{options.json_filename=}')
+
+
+def _report(options):
+    logging.info(f'Starting a report...')
+    logging.debug(f'{options.date=}')
+
+
+def runner(options):
+    logging.debug(f'Starting runner...\n{options=}')
+    return COMMANDS[options.command](options)
+
+
+COMMANDS = {
+    'reset': _reset,
+    'import': _import_json_file,
+    'report': _report,
+}
 
 if __name__ == "__main__":
     import sys
-    main(sys.argv)
+    args = _parse(sys.argv)
+    logging.basicConfig(level=logging.DEBUG if args.debug else logging.INFO)
+
+    runner(args)
